b7a14bea0c226ec28845613f50dae68771ece1c0
Rony2518 <113433588+Rony2518@users.noreply.github.com>
2023-06-25 18:43:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let info = document.querySelector('.info');
-let contact = document.querySelector('.contact');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-x');
     navbar.classList.toggle('open');
     info.classList.toggle('shift-down1');
-    contact.classList.toggle('shift-down2');
 }
+
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the necessary elements
+    var contact = document.querySelector(".contact");
+    var menuIcon = document.getElementById("menu-icon");
+
+    // Add event listener to the menu icon
+    menuIcon.addEventListener("click", function () {
+        // Toggle the active class on the contact element
+        contact.classList.toggle("active");
+    });
+});
+
 
 var icon = document.getElementById("icon");
 icon.onclick = function () {
